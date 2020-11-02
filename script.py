2a33c67475234afccfa49bed28624daa215daa54
suthar0052 <suthar0052@gmail.com>
2020-11-02 19:31:06 +0530

    modifed to show for the currernt year

--- script.py
+++ script.py
@@ -5,70 +5,83 @@ from bokeh.resources import CDN
 from bokeh.embed import components
 from datetime import datetime
 
+from pandas_datareader._utils import RemoteDataError
+
 app = Flask(__name__)
 
 script1, div1 = ('', '')
 js_files, css_files = '', ''
+error = ''
 
 
 @app.route('/plot', methods=['POST'])
 def draw_plot():
-    print(request.method)
     if request.method == 'POST':
-        date = datetime.now().strftime("%Y,%m,%d")
-        date = date.split(",")
-        year = int(date[0])
-        month = int(date[1])
-        day = int(date[2])
-        code = request.form['code']
-        start = datetime(year, month - 3, 1)
+        try:
+            date = datetime.now().strftime("%Y,%m,%d")
+            date = date.split(",")
+            year = int(date[0])
+            month = int(date[1])
+            day = int(date[2])
+            code = request.form['code']
+            start = datetime(year, month - 3, 1)
+            global error
+            error = 'please enter correct code'
 
-        end = datetime(year, month, day)
+            end = datetime(year, month, day)
 
-        # print(start)
-        # end = datetime.time(2020,10,25)
-        # help(datetime)
-        df = data.DataReader(code.upper(), 'yahoo', start=start, end=end)
+            # print(start)
+            # end = datetime.time(2020,10,25)
+            # help(datetime)
+            df = data.DataReader(code.upper(), 'yahoo', start=start, end=end)
 
-        # df.index[df.Close > df.Open]
+            # if code == '' or df:
 
-        def inc_dec(c, o):
-            if c > o:
-                return "Increase"
-            elif c < o:
-                return "Decrease"
-            else:
-                return "Equal"
+            # df.index[df.Close > df.Open]
 
-        df["Status"] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
-        df["Middle"] = (df.Open + df.Close) / 2
-        df["Width"] = 12 * 60 * 60 * 1000
-        df["Height"] = abs(df.Open - df.Close)
-        df
+            def inc_dec(c, o):
+                if c > o:
+                    return "Increase"
+                elif c < o:
+                    return "Decrease"
+                else:
+                    return "Equal"
 
-        f = figure(x_axis_type="datetime", width=1000, height=700, sizing_mode="scale_width")
+            df["Status"] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
+            df["Middle"] = (df.Open + df.Close) / 2
+            df["Width"] = 12 * 60 * 60 * 1000
+            df["Height"] = abs(df.Open - df.Close)
+            df
 
-        f.title.text = "Candlestick Graph for "+code.upper()
+            f = figure(x_axis_type="datetime", width=1000, height=700, sizing_mode="scale_width")
 
-        f.grid.grid_line_alpha = 0.3
+            f.title.text = "Candlestick Graph for " + code.upper()
 
-        f.segment(df.index, df.High, df.index, df.Low, line_color="black", line_width=1)
+            f.grid.grid_line_alpha = 0.3
 
-        f.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"], df.Width[df.Status == "Increase"],
-               df.Height[df.Status == "Increase"], fill_color="green", line_color='black')
-        f.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"], df.Width[df.Status == "Decrease"],
-               df.Height[df.Status == "Decrease"], fill_color="red", line_color='black')
-        # help(f.rect)
+            f.segment(df.index, df.High, df.index, df.Low, line_color="black", line_width=1)
 
-        global script1, div1, js_files, css_files
+            f.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"],
+                   df.Width[df.Status == "Increase"],
+                   df.Height[df.Status == "Increase"], fill_color="green", line_color='black')
+            f.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"],
+                   df.Width[df.Status == "Decrease"],
+                   df.Height[df.Status == "Decrease"], fill_color="red", line_color='black')
+            # help(f.rect)
 
-        js_files = CDN.js_files[0]
-        script1, div1 = components(f)
-        js_files = CDN.js_files[0]
-        # print(components(f))
-        # output_file("data.html")
-        # show(f)
-        return render_template("plot.html", script1=script1, div1=div1, js_files=js_files, css_files=css_files)
+            global script1, div1, js_files, css_files
+
+            js_files = CDN.js_files[0]
+            script1, div1 = components(f)
+            js_files = CDN.js_files[0]
+            # print(components(f))
+            # output_file("data.html")
+            # show(f)
+            return render_template("plot.html", script1=script1, div1=div1, js_files=js_files, css_files=css_files)
+        except RemoteDataError as error:
+            error = error
+            return render_template("plot.html", script1=script1, div1=div1, js_files=js_files, css_files=css_files,
+                                   error=error)
 
 
 @app.route('/plot')
