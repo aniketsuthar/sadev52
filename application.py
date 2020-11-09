7e56a8990b33e1f85046e012353a55a132d57b7a
suthar0052 <suthar0052@gmail.com>
2020-11-09 16:51:49 +0530

    init

--- application.py
+++ application.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, request
 from pandas_datareader import data
+from pandas import DataFrame
 from bokeh.plotting import figure, show, output_file
 from bokeh.resources import CDN
 from bokeh.embed import components
+from bokeh.models import LabelSet, HoverTool, ColumnDataSource
 from datetime import datetime
-
 from pandas_datareader._utils import RemoteDataError
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 script1, div1 = ('', '')
 js_files, css_files = '', ''
 error = ''
 
 
-@app.route('/plot', methods=['POST'])
+@application.route('/plot', methods=['POST'])
 def draw_plot():
     if request.method == 'POST':
         try:
@@ -24,15 +25,12 @@ def draw_plot():
             month = int(date[1])
             day = int(date[2])
             code = request.form['code']
-            start = datetime(year, month - 3, 1)
+            start = datetime(year, month - 1, 1)
             global error
             error = 'please enter correct code'
 
             end = datetime(year, month, day)
 
-            # print(start)
-            # end = datetime.time(2020,10,25)
-            # help(datetime)
             df = data.DataReader(code.upper(), 'yahoo', start=start, end=end)
 
             # if code == '' or df:
@@ -51,32 +49,36 @@ def draw_plot():
             df["Middle"] = (df.Open + df.Close) / 2
             df["Width"] = 12 * 60 * 60 * 1000
             df["Height"] = abs(df.Open - df.Close)
-            df
 
             f = figure(x_axis_type="datetime", width=1000, height=700, sizing_mode="scale_width")
-
+            f.xaxis.axis_label = 'Date'
+            f.yaxis.axis_label = 'Price in $'
             f.title.text = "Candlestick Graph for " + code.upper()
 
             f.grid.grid_line_alpha = 0.5
+            cds = ColumnDataSource(df)
+            cds1 = ColumnDataSource(df[df.Status == "Increase"])
+            cds2 = ColumnDataSource(df[df.Status == "Decrease"])
+            tooltip = HoverTool(
+                tooltips=[("High", "@High{111.11111}"), ("Low", "@Low{111.11111}"), ("Open", "@Open{111.111111}"),
+                          ("Close", "@Close{111.11111}")])
 
-            f.segment(df.index, df.High, df.index, df.Low, line_color="black", line_width=1)
+            f.add_tools(tooltip)
 
-            f.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"],
-                   df.Width[df.Status == "Increase"],
-                   df.Height[df.Status == "Increase"], fill_color="green", line_color='black')
-            f.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"],
-                   df.Width[df.Status == "Decrease"],
-                   df.Height[df.Status == "Decrease"], fill_color="red", line_color='black')
-            # help(f.rect)
+            f.segment(x0="Date", y0="High", x1="Date", y1="Low", line_color="black", line_width=1, source=cds)
 
+            f.rect(x="Date", y="Middle",
+                   width="Width",
+                   height="Height", fill_color="green", line_color='black', source=cds1)
+
+            f.rect(x="Date", y="Middle",
+                   width="Width",
+                   height="Height", fill_color="red", line_color='black', source=cds2)
             global script1, div1, js_files, css_files
 
             js_files = CDN.js_files[0]
             script1, div1 = components(f)
             js_files = CDN.js_files[0]
-            # print(components(f))
-            # output_file("data.html")
-            # show(f)
             return render_template("plot.html", script1=script1, div1=div1, js_files=js_files, css_files=css_files)
         except RemoteDataError as error:
             error = error
@@ -84,20 +86,20 @@ def draw_plot():
                                    error=error)
 
 
-@app.route('/plot')
+@application.route('/plot')
 def plot():
     return render_template("plot.html", script1=script1, div1=div1, js_files=js_files, css_files=css_files)
 
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template("/home.html")
 
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template("/about.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
